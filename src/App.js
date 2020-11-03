872df623d3b34706695adddc33cfd8483d176524
emilbergs <emilberg@outlook.dk>
2020-11-03 17:11:46 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
+import data from "./data.json";
 
 function App() {
   return (
-    <div classname="App">
-      Test MAP
+    <div className="App">
+      <div className="posts">
+        {data.transactions.map(post => {
+          return (
+            <>
+              <img src={post.iconURL} alt="icon" />
+              <h1>{post.type}</h1>
+              <h2>{post.localizableTitle}</h2>
+              <h2>{post.billingAmount.amount + ' ' + post.billingAmount.currency}</h2>
+              <h2>{post.localizableTitle}</h2>
+
+
+
+
+            </>
+          )
+        })}
+      </div>
     </div>
   );
 }
